2142a171db29035e547aa0d855274750db40b1db
Roman Plis <plisroman@gmail.com>
2026-01-29 22:02:12 +0500

    init: start new task

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -17,8 +17,15 @@
  * 0    => promise that will be fulfilled
  * 1    => promise that will be fulfilled
  */
-function getPromise(/* number */) {
-  throw new Error('Not implemented');
+function getPromise(number) {
+  const prms = new Promise((resolve, reject) => {
+    if (number >= 0) {
+      resolve(number);
+      return;
+    }
+    reject(new Error());
+  });
+  return prms;
 }
 
 /**
